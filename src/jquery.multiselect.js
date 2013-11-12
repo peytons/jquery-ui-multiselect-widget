c9d90220ad97dc82f48b17079843d9283b04d0d4
Peyton Sherwood <peytons@gmail.com>
2013-11-11 18:25:42 -0600

    even more

--- src/jquery.multiselect.js
+++ src/jquery.multiselect.js
@@ -68,7 +68,7 @@
           .appendTo(button),
 
         menu = (this.menu = $('<div />'))
-          .addClass('ui-multiselect-menu ui-widget ui-widget-content ui-corner-all')
+          .addClass('ui-multiselect-menu ui-corner-all')
           .addClass(o.classes)
           .appendTo($(o.appendTo)),
 
